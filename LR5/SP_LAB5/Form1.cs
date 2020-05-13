fc390d20fbd381ec86a9f8a02f92c8db6d15e129
KhasV <khasanovarvara@gmail.com>
2020-05-13 20:29:31 +0300

    labki

---
+++ LR5/SP_LAB5/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SP_LAB5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
